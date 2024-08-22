01ff5d8c349ffa18593d4820fac9cad72e249ed2
GENIE <hyunjin117@naver.com>
2024-08-22 23:45:41 +0900

    ➕Plugin: lint tool Spotless 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     kotlin("plugin.spring") version "1.9.24"
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
+    // lint plugin Spotless
+    id("com.diffplug.spotless") version "6.21.0"
 }
 
 group = "com.example"
@@ -40,6 +42,26 @@ kotlin {
     }
 }
 
+tasks.named("compileJava") {
+    dependsOn(tasks.named("spotlessApply"))
+}
+
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+        target("src/**/*.kt") // 적용할 파일 범위
+        // 기본적인 Kotlin 스타일 적용
+        ktlint("0.49.1").userData(mapOf("disabled_rules" to "no-wildcard-imports"))
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+
+    format("misc") {
+        target("**/*.gradle", "**/*.md", "**/*.yml", "**/*.yaml", "**/.gitignore")
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
 }
